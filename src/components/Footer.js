449a5fbd8618a824d49c56099bfa2f122106e12d
Tristan LaRoche <laroche11235@gmail.com>
2021-06-01 19:55:13 -0400

    change icon to text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 
-import linkedinB from '../Images/social-icons/linkedin-b.png'
-import gitHubB from '../Images/social-icons/github-b.png'
 
 function Footer() {
 
@@ -14,9 +12,9 @@ function Footer() {
             <Container fluid={true}>
                 <Row className="border-top justify-content-between align-items-center p-3">
                     <Col className="p-0 md={3} sm={12}">
-                    <a href="https://www.linkedin.com/in/tristan-laroche/" target="_blank"><img className="footer-navIcons" src={linkedinB}  alt="linkedin icon."/></a>
+                    <a href="https://www.linkedin.com/in/tristan-laroche/" target="_blank">My Linkedin</a><br></br>
 
-                    <a href="https://github.com/taroche" target="_blank"><img className="footer-navIcons" src={gitHubB}  alt="github icon."/></a>
+                    <a href="https://github.com/taroche" target="_blank">My Github</a>
                     </Col>
                     <Col className="p-0 d-flex justify-content-end font-weight-light md={3}">
                         This site was made by Tristan LaRoche.
